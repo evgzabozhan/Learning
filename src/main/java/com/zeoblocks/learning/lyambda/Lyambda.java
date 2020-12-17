d3ca437f8ea07e4c5fa8e7bfdd6c18f674169780
ezabozhan <ezabozhan@fmlogistic.com>
2020-12-17 08:18:37 +0300

    Learning Lambda

--- src/main/java/com/zeoblocks/learning/lyambda/Lyambda.java
+++ src/main/java/com/zeoblocks/learning/lyambda/Lyambda.java
@@ -1,14 +1,14 @@
 package com.zeoblocks.learning.lyambda;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Lyambda {
     public static void main(String[] args) {
         String[] words = { "Hello1","Hello2","Hello3","World4","World"};
 
         Arrays.sort(words,(first,second) ->first.length() - second.length());
-
-        System.out.println(words.toString());
+        Arrays.sort(words, Comparator.comparingInt(String::length));
 
     }
 }
